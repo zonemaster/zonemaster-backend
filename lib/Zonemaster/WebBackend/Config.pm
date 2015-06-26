f2a3f5a3e24e6409db679ed33564edc70222edd3
Patrik Wallström <pawal@blipp.com>
2015-06-26 09:55:56 +0200

    Bump to version 1.0.3

--- lib/Zonemaster/WebBackend/Config.pm
+++ lib/Zonemaster/WebBackend/Config.pm
@@ -1,5 +1,5 @@
 package Zonemaster::WebBackend::Config;
-our $VERSION = '1.0.2_01';
+our $VERSION = '1.0.3';
 
 use strict;
 use warnings;
--- lib/Zonemaster/WebBackend/DB.pm
+++ lib/Zonemaster/WebBackend/DB.pm
@@ -1,6 +1,6 @@
 package Zonemaster::WebBackend::DB;
 
-our $VERSION = '1.0.2_01';
+our $VERSION = '1.0.3';
 
 use Moose::Role;
 

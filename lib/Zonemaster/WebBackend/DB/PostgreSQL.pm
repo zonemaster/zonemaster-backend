102cad1bb01250906c8a54ddee3598dd3dbb35fb
Michal TOMA <toma@afnic.fr>
2015-09-23 14:00:25 +0200

    First version of non numeris id for test results

--- lib/Zonemaster/WebBackend/DB/PostgreSQL.pm
+++ lib/Zonemaster/WebBackend/DB/PostgreSQL.pm
@@ -68,10 +68,11 @@ sub user_authorized {
 sub test_progress {
     my ( $self, $test_id, $progress ) = @_;
 
+	my $id_field = $self->_get_allowed_id_field_name($test_id);
     my $dbh = $self->dbh;
-    $dbh->do( "UPDATE test_results SET progress=$progress WHERE id=?", undef, $test_id ) if ( $progress );
-
-    my ( $result ) = $dbh->selectrow_array( "SELECT progress FROM test_results WHERE id=?", undef, $test_id );
+    $dbh->do( "UPDATE test_results SET progress=$progress WHERE $id_field=?", undef, $test_id ) if ( $progress );
+	
+    my ( $result ) = $dbh->selectrow_array( "SELECT progress FROM test_results WHERE $id_field=?", undef, $test_id );
 
     return $result;
 }
@@ -121,8 +122,15 @@ sub create_new_test {
 
     my $nb_inserted = $dbh->do( $query );
 
-    ( $result ) = $dbh->selectrow_array(
-        "SELECT MAX(id) AS id FROM test_results WHERE params_deterministic_hash='$test_params_deterministic_hash'" );
+    my ( $id, $hash_id ) = $dbh->selectrow_array(
+        "SELECT id, hash_id FROM test_results WHERE params_deterministic_hash='$test_params_deterministic_hash' ORDER BY id DESC LIMIT 1" );
+        
+    if ( $id > Zonemaster::WebBackend::Config->force_hash_id_use_in_API_starting_from_id() ) {
+		$result = $hash_id;
+    }
+    else {
+		$result = $id;
+    }
 
     return $result;
 }
@@ -132,7 +140,8 @@ sub get_test_params {
 
     my $result;
 
-    my ( $params_json ) = $self->dbh->selectrow_array( "SELECT params FROM test_results WHERE id=?", undef, $test_id );
+	my $id_field = $self->_get_allowed_id_field_name($test_id);
+    my ( $params_json ) = $self->dbh->selectrow_array( "SELECT params FROM test_results WHERE $id_field=?", undef, $test_id );
     eval { $result = decode_json( encode_utf8( $params_json ) ); };
     die $@ if $@;
 
@@ -142,13 +151,14 @@ sub get_test_params {
 sub test_results {
     my ( $self, $test_id, $results ) = @_;
 
-    $self->dbh->do( "UPDATE test_results SET progress=100, test_end_time=NOW(), results = ? WHERE id=?",
+	my $id_field = $self->_get_allowed_id_field_name($test_id);
+    $self->dbh->do( "UPDATE test_results SET progress=100, test_end_time=NOW(), results = ? WHERE $id_field=?",
         undef, $results, $test_id )
       if ( $results );
 
     my $result;
     eval {
-        my ( $hrefs ) = $self->dbh->selectall_hashref( "SELECT * FROM test_results WHERE id=?", 'id', undef, $test_id );
+        my ( $hrefs ) = $self->dbh->selectall_hashref( "SELECT * FROM test_results WHERE $id_field=?", $id_field, undef, $test_id );
         $result            = $hrefs->{$test_id};
         $result->{params}  = decode_json( encode_utf8( $result->{params} ) );
         $result->{results} = decode_json( encode_utf8( $result->{results} ) );
@@ -158,23 +168,13 @@ sub test_results {
     return $result;
 }
 
-sub get_test_request {
-    my ( $self ) = @_;
-
-    my $dbh = $self->dbh;
-    my ( $id ) =
-      $dbh->selectrow_array( q[ SELECT id FROM test_results WHERE progress=0 ORDER BY priority ASC, id ASC LIMIT 1 ] );
-    $dbh->do( q[UPDATE test_results SET progress=1 WHERE id=?], undef, $id );
-
-    return $id;
-}
-
 sub get_test_history {
     my ( $self, $p ) = @_;
 
     $p->{offset} //= 0;
     $p->{limit} //= 200;
 
+    my $use_hash_id_from_id = Zonemaster::WebBackend::Config->force_hash_id_use_in_API_starting_from_id();
     my $undelegated =
         ( defined $p->{frontend_params}->{nameservers} )
       ? ( "AND (params->'nameservers') IS NOT NULL" )
@@ -186,39 +186,40 @@ sub get_test_history {
 			(SELECT count(*) FROM (SELECT json_array_elements(results) AS result) AS t1 WHERE result->>'level'='CRITICAL') AS nb_critical,
 			(SELECT count(*) FROM (SELECT json_array_elements(results) AS result) AS t1 WHERE result->>'level'='ERROR') AS nb_error,
 			(SELECT count(*) FROM (SELECT json_array_elements(results) AS result) AS t1 WHERE result->>'level'='WARNING') AS nb_warning,
-			id, 
+			id,
+			hash_id,
 			creation_time, 
 			params->>'advanced_options' AS advanced_options 
 		FROM test_results 
 		WHERE params->>'domain'=" . $self->dbh->quote( $p->{frontend_params}->{domain} ) . " $undelegated 
 		ORDER BY id DESC 
 		OFFSET $p->{offset} LIMIT $p->{limit}";
-    my $sth1 = $self->dbh->prepare( $query );
-    $sth1->execute;
-    while ( my $h = $sth1->fetchrow_hashref ) {
-        my $overall_result = 'ok';
-        if ( $h->{nb_critical} ) {
-            $overall_result = 'critical';
-        }
-        elsif ( $h->{nb_error} ) {
-            $overall_result = 'error';
-        }
-        elsif ( $h->{nb_warning} ) {
-            $overall_result = 'warning';
-        }
+	my $sth1 = $self->dbh->prepare( $query );
+	$sth1->execute;
+	while ( my $h = $sth1->fetchrow_hashref ) {
+		my $overall_result = 'ok';
+		if ( $h->{nb_critical} ) {
+			$overall_result = 'critical';
+		}
+		elsif ( $h->{nb_error} ) {
+			$overall_result = 'error';
+		}
+		elsif ( $h->{nb_warning} ) {
+			$overall_result = 'warning';
+		}
 
-        push(
-            @results,
-            {
-                id               => $h->{id},
-                creation_time    => $h->{creation_time},
-                advanced_options => $h->{advanced_options},
-                overall_result   => $overall_result
-            }
-        );
-    }
+		push(
+			@results,
+			{
+				id               => ($h->{id} > $use_hash_id_from_id)?($h->{hash_id}):($h->{id}),
+				creation_time    => $h->{creation_time},
+				advanced_options => $h->{advanced_options},
+				overall_result   => $overall_result
+			}
+		);
+	}
 
-    return \@results;
+	return \@results;
 }
 
 no Moose;

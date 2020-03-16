a7f247af0e406c61af86daed09ed2eedf6bf0610
Michal TOMA <toma@nic.fr>
2020-03-16 15:36:38 +0100

    Added a coment and a pointer to ma more detailed description of the issue

--- lib/Zonemaster/Backend/DB/PostgreSQL.pm
+++ lib/Zonemaster/Backend/DB/PostgreSQL.pm
@@ -186,6 +186,8 @@ sub test_results {
         my ( $hrefs ) = $dbh->selectall_hashref( "SELECT id, hash_id, creation_time at time zone current_setting('TIMEZONE') at time zone 'UTC' as creation_time, params, results FROM test_results WHERE $id_field=?", $id_field, undef, $test_id );
         $result            = $hrefs->{$test_id};
         
+        # This workaround is needed to properly handle all versions of perl and the DBD::Pg module 
+        # More details in the zonemaster backend issue #570
         if (utf8::is_utf8($result->{params}) ) {
                 $result->{params}  = decode_json( encode_utf8($result->{params}) );
         }

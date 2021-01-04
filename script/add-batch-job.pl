ae8be3bf7e62d26134be570f4708cbd9cb195091
Mats Dufberg <mats.dufberg@iis.se>
2021-01-04 16:46:57 +0100

    Editorial updates
    * Adds a short comment (documentation) of the purpose of the
      script.
    * Replaces all TABs with appropriate number of SPACEs.
    * Justifies all lines.

--- script/add-batch-job.pl
+++ script/add-batch-job.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+# This script is for testing purpose only.
+
 use 5.14.2;
 use warnings;
 
@@ -15,7 +17,7 @@ my $e = Zonemaster::Backend::RPCAPI->new;
 say "Starting add_batch_job";
 my @domains;
 for (my $i = 0; $i < 100; $i++) {
-	push(@domains, substr(md5_hex(rand(10000)), 0, 5).".fr");
+    push(@domains, substr(md5_hex(rand(10000)), 0, 5).".fr");
 }
 
 #die Dumper(\@domains);
@@ -23,18 +25,18 @@ for (my $i = 0; $i < 100; $i++) {
 $e->add_api_user({ username => 'test_user', api_key => 'API_KEY_01'});
 
 $e->add_batch_job(
-	{
-		client_id      => 'Add Script',
-		client_version => '1.0',
-		username       => 'test_user',
-		api_key        => 'API_KEY_01',
-		test_params    => {
+    {
+        client_id      => 'Add Script',
+        client_version => '1.0',
+        username       => 'test_user',
+        api_key        => 'API_KEY_01',
+        test_params    => {
             client_id      => 'Add Script',
             client_version => '1.0',
             ipv4           => 1,                   # 0 or 1, is the ipv4 checkbox checked
             ipv6           => 1,                   # 0 or 1, is the ipv6 checkbox checked
-            profile        => 'default',    # the id if the Test profile listbox (unused)
+            profile        => 'default',           # the id if the Test profile listbox (unused)
         },
         domains => \@domains,
-	}
-);
+    }
+    );

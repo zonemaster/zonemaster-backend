795138e8d0023e51477653e6af4fc6bfb70b7d3c
Mattias Päivärinta <mattias.paivarinta@iis.se>
2021-03-18 18:01:27 +0000

    Clean up commented out code

--- lib/Zonemaster/Backend/DB/PostgreSQL.pm
+++ lib/Zonemaster/Backend/DB/PostgreSQL.pm
@@ -39,8 +39,6 @@ sub dbh {
         my $connection_user     = $self->config->DB_user();
         my $connection_password = $self->config->DB_password();
         $dbh = DBI->connect( $connection_string, $connection_user, $connection_password, $connection_args );
-#        $dbh->{InactiveDestroy} = 1;
-# This line vas introduced to fix a non-trivial, hard to reproduce problem, it is causing giant memory leaks. It is kept here commented out for a while in case the initial problem occurs again.
         $dbh->{AutoInactiveDestroy} = 1;
         $self->dbhandle( $dbh );
         return $dbh;

7e39c0fe7775a0a23ab388c55f7a9fac3424d2eb
Mattias Päivärinta <mattias.paivarinta@iis.se>
2021-04-21 14:02:32 +0200

    Refactor Validator

--- lib/Zonemaster/Backend/Validator.pm
+++ lib/Zonemaster/Backend/Validator.pm
@@ -7,6 +7,22 @@ use warnings;
 use 5.14.2;
 
 use JSON::Validator::Joi;
+use Readonly;
+
+Readonly my $IPV4_RE => qr/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\$/;
+Readonly my $IPV6_RE => qr/^([0-9A-Fa-f]{1,4}:[0-9A-Fa-f:]{1,}(:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})?)\$|([0-9A-Fa-f]{1,4}::[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\$/;
+
+Readonly my $API_KEY_RE             => qr/^[a-zA-Z0-9-_]{1,512}$/;
+Readonly my $CLIENT_ID_RE           => qr/^[a-zA-Z0-9-+~_.: ]{1,50}$/;
+Readonly my $CLIENT_VERSION_RE      => qr/^[a-zA-Z0-9-+~_.: ]{1,50}$/;
+Readonly my $DIGEST_RE              => qr/^[A-Fa-f0-9]{40}\$|^[A-Fa-f0-9]{64}\$/;
+Readonly my $IPADDR_RE              => qr/^$|$IPV4_RE|$IPV6_RE/;
+Readonly my $JSONRPC_METHOD_RE      => qr/^[a-zA-Z0-9_-]*$/;
+Readonly my $LANGUAGE_RE            => qr/^[a-z]{2}(_[A-Z]{2})?$/;
+Readonly my $PROFILE_NAME_RE        => qr/^[a-zA-Z0-9]$|^[a-zA-Z0-9][a-zA-Z0-9_-]{0,30}[a-zA-Z0-9]$/;
+Readonly my $RELAXED_DOMAIN_NAME_RE => qr/^[.]$|^.{2,254}$/;
+Readonly my $TEST_ID_RE             => qr/^[0-9a-f]{16}$/;
+Readonly my $USERNAME_RE            => qr/^[a-zA-Z0-9-.@]{1,50}$/;
 
 sub joi {
     return JSON::Validator::Joi->new;
@@ -21,34 +37,31 @@ sub new {
     return ( $self );
 }
 
-my $ipv4_regex = "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\$";
-my $ipv6_regex = "^([0-9A-Fa-f]{1,4}:[0-9A-Fa-f:]{1,}(:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})?)\$|([0-9A-Fa-f]{1,4}::[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\$";
-
 sub api_key {
-    return joi->string->regex('^[a-zA-Z0-9-_]{1,512}$');
+    return joi->string->regex( $API_KEY_RE );
 }
 sub batch_id {
     return joi->integer->positive;
 }
 sub client_id {
-    return joi->string->regex('^[a-zA-Z0-9-+~_.: ]{1,50}$');
+    return joi->string->regex( $CLIENT_ID_RE );
 }
 sub client_version {
-    return joi->string->regex('^[a-zA-Z0-9-+~_.: ]{1,50}$');
+    return joi->string->regex( $CLIENT_VERSION_RE );
 }
 sub domain_name {
-    return joi->string->regex('^[.]$|^.{2,254}$');
+    return joi->string->regex( $RELAXED_DOMAIN_NAME_RE );
 }
 sub ds_info {
     return joi->object->strict->props(
-        digest => joi->string->regex("^[A-Fa-f0-9]{40}\$|^[A-Fa-f0-9]{64}\$")->required,
+        digest => joi->string->regex($DIGEST_RE)->required,
         algorithm => joi->integer->min(0)->required,
         digtype => joi->integer->min(0)->required,
         keytag => joi->integer->min(0)->required,
     );
 }
 sub ip_address {
-    return joi->string->regex('^$|'.$ipv4_regex."|".$ipv6_regex);
+    return joi->string->regex( $IPADDR_RE );
 }
 sub nameserver {
     return joi->object->strict->props(
@@ -60,20 +73,20 @@ sub priority {
     return joi->integer;
 }
 sub profile_name {
-    return joi->string->regex('^[a-zA-Z0-9]$|^[a-zA-Z0-9][a-zA-Z0-9_-]{0,30}[a-zA-Z0-9]$');
+    return joi->string->regex( $PROFILE_NAME_RE );
 }
 sub queue {
     return joi->integer;
 }
 sub test_id {
-    return joi->string->regex('^[0-9a-f]{16}$');
+    return joi->string->regex( $TEST_ID_RE );
 }
 sub language_tag {
-    return joi->string->regex('^[a-z]{2}(_[A-Z]{2})?$');
+    return joi->string->regex( $LANGUAGE_RE );
 }
 sub username {
-    return joi->string->regex('^[a-zA-Z0-9-.@]{1,50}$');
+    return joi->string->regex( $USERNAME_RE );
 }
 sub jsonrpc_method {
-    return joi->string->regex('^[a-zA-Z0-9_-]*$');
+    return joi->string->regex( $JSONRPC_METHOD_RE );
 }

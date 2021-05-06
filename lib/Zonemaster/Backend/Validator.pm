6c0b3f0f5d54f157256ee00a07c3a39b07a19432
Mattias Päivärinta <mattias.paivarinta@iis.se>
2021-05-06 12:04:25 +0200

    Refactor regexes to use /i

--- lib/Zonemaster/Backend/Validator.pm
+++ lib/Zonemaster/Backend/Validator.pm
@@ -10,19 +10,19 @@ use JSON::Validator::Joi;
 use Readonly;
 
 Readonly my $IPV4_RE => qr/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\$/;
-Readonly my $IPV6_RE => qr/^([0-9A-Fa-f]{1,4}:[0-9A-Fa-f:]{1,}(:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})?)\$|([0-9A-Fa-f]{1,4}::[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\$/;
+Readonly my $IPV6_RE => qr/^([0-9a-f]{1,4}:[0-9a-f:]{1,}(:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})?)\$|([0-9a-f]{1,4}::[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\$/i;
 
-Readonly my $API_KEY_RE             => qr/^[a-zA-Z0-9-_]{1,512}$/;
-Readonly my $CLIENT_ID_RE           => qr/^[a-zA-Z0-9-+~_.: ]{1,50}$/;
-Readonly my $CLIENT_VERSION_RE      => qr/^[a-zA-Z0-9-+~_.: ]{1,50}$/;
-Readonly my $DIGEST_RE              => qr/^[A-Fa-f0-9]{40}\$|^[A-Fa-f0-9]{64}\$/;
+Readonly my $API_KEY_RE             => qr/^[a-z0-9-_]{1,512}$/i;
+Readonly my $CLIENT_ID_RE           => qr/^[a-z0-9-+~_.: ]{1,50}$/i;
+Readonly my $CLIENT_VERSION_RE      => qr/^[a-z0-9-+~_.: ]{1,50}$/i;
+Readonly my $DIGEST_RE              => qr/^[a-f0-9]{40}\$|^[a-f0-9]{64}\$/i;
 Readonly my $IPADDR_RE              => qr/^$|$IPV4_RE|$IPV6_RE/;
-Readonly my $JSONRPC_METHOD_RE      => qr/^[a-zA-Z0-9_-]*$/;
+Readonly my $JSONRPC_METHOD_RE      => qr/^[a-z0-9_-]*$/i;
 Readonly my $LANGUAGE_RE            => qr/^[a-z]{2}(_[A-Z]{2})?$/;
-Readonly my $PROFILE_NAME_RE        => qr/^[a-zA-Z0-9]$|^[a-zA-Z0-9][a-zA-Z0-9_-]{0,30}[a-zA-Z0-9]$/;
+Readonly my $PROFILE_NAME_RE        => qr/^[a-z0-9]$|^[a-z0-9][a-z0-9_-]{0,30}[a-z0-9]$/i;
 Readonly my $RELAXED_DOMAIN_NAME_RE => qr/^[.]$|^.{2,254}$/;
 Readonly my $TEST_ID_RE             => qr/^[0-9a-f]{16}$/;
-Readonly my $USERNAME_RE            => qr/^[a-zA-Z0-9-.@]{1,50}$/;
+Readonly my $USERNAME_RE            => qr/^[a-z0-9-.@]{1,50}$/i;
 
 sub joi {
     return JSON::Validator::Joi->new;

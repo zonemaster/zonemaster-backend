b12c98f15e5be7622abd79b15efc3171f9fa81b9
Mats Dufberg <mats.dufberg@iis.se>
2021-07-14 14:46:56 +0200

    Updates PostgreSQL
    * Adds field "undelegated" to the database schema to match the
      other databases.
    * Corrects the support for delegated/undelegated in the database
      engine.

--- share/initial-postgres.sql
+++ share/initial-postgres.sql
@@ -16,6 +16,7 @@ CREATE TABLE test_results (
     progress integer DEFAULT 0,
     params_deterministic_hash varchar(32),
     params json NOT NULL,
+    undelegated boolean NOT NULL DEFAULT false,
     results json,
     nb_retries integer NOT NULL DEFAULT 0
 );

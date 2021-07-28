8cc5186dab68adb94929a676b951e280b72caab8
Gaël Berthaud-Müller <gael.berthaud-muller@afnic.fr>
2021-07-28 13:08:55 +0200

    add scripts to backfill undelegated fields in db

--- share/patch_postgresql_db_zonemaster_backend_ver_7.0.0.pl
+++ share/patch_postgresql_db_zonemaster_backend_ver_7.0.0.pl
@@ -20,8 +20,56 @@ sub patch_db {
     ####################################################################
     # TEST RESULTS
     ####################################################################
-    $dbh->do( 'ALTER TABLE test_results ADD COLUMN undelegated integer NOT NULL DEFAULT 0' );
+    $dbh->do( 'ALTER TABLE test_results ADD COLUMN IF NOT EXISTS undelegated integer NOT NULL DEFAULT 0' );
+    $dbh->do( qq[
+update test_results set undelegated = test_results_undelegated.undelegated_bool::int
+from (
+    select
+        test_results.id,
+        (
+            case when ds_filled.ds_filled is null then false else ds_filled.ds_filled end
+            or
+            case when ns_filled.ns_filled is null then false else ns_filled.ns_filled end
+        ) as undelegated_bool
+    from test_results
+    left join (
+        select
+            count(*) > 0 as ds_filled,
+            id
+        from (
+            select
+                jd.value,
+                id
+            from
+            (
+                select json_array_elements(params->'ds_info') as ja, id
+                from test_results
+            )  as s1,
+            json_each_text(ja) as jd
+        ) as s2
+        where value is not null and value::text != ''::text
+        group by id
+    ) as ds_filled on ds_filled.id = test_results.id
+    left join (
+            select
+            count(*) > 0 as ns_filled,
+            id
+        from (
+            select
+                jd.value,
+                id
+            from
+            (
+                select json_array_elements(params->'nameservers') as ja, id
+                from test_results
+            )  as s1,
+            json_each_text(ja) as jd
+        ) as s2
+        where value is not null and value::text != ''::text
+        group by id
+    ) as ns_filled on ns_filled.id = test_results.id
+) as test_results_undelegated where test_results.id = test_results_undelegated.id;
+    ] );
 }
 
 patch_db();
-

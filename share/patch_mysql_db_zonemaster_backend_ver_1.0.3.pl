d77e5e6de21f0f9360c42dca8bed40b168f8a4b1
Alexandre Pion <pion@afnic.fr>
2021-09-29 12:25:54 +0200

    Use spaces instead of tabs

--- share/patch_mysql_db_zonemaster_backend_ver_1.0.3.pl
+++ share/patch_mysql_db_zonemaster_backend_ver_1.0.3.pl
@@ -22,18 +22,18 @@ sub patch_db {
     $dbh->do( 'UPDATE test_results SET hash_id = (SELECT SUBSTRING(MD5(CONCAT(RAND(), UUID())) from 1 for 16))' );
 
     $dbh->do( 'ALTER TABLE test_results MODIFY hash_id VARCHAR(16) DEFAULT NULL NOT NULL' );
-    
+
     $dbh->do(
-		'CREATE TRIGGER before_insert_test_results
-			BEFORE INSERT ON test_results
-			FOR EACH ROW
-			BEGIN
-				IF new.hash_id IS NULL OR new.hash_id=\'\'
-				THEN
-					SET new.hash_id = SUBSTRING(MD5(CONCAT(RAND(), UUID())) from 1 for 16);
-				END IF;
-			END;
-		'
+        'CREATE TRIGGER before_insert_test_results
+            BEFORE INSERT ON test_results
+            FOR EACH ROW
+            BEGIN
+                IF new.hash_id IS NULL OR new.hash_id=\'\'
+                THEN
+                    SET new.hash_id = SUBSTRING(MD5(CONCAT(RAND(), UUID())) from 1 for 16);
+                END IF;
+            END;
+        '
     );
 }
 

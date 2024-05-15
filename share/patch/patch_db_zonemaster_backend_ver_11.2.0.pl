0c722ea348d74dda293a0baf11735b081aa22533
Marc van der Wal <marc.vanderwal@afnic.fr>
2024-05-15 11:08:08 +0200

    Fix module name BACKEND_TEST_AGENT in database
    With the previous changes, “BACKEND_TEST_AGENT” should have been stored
    as “Backend_Test_Agent” in the database. This migration script ensures
    everything is consistent again.

---
+++ share/patch/patch_db_zonemaster_backend_ver_11.2.0.pl
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+
+use List::MoreUtils qw(zip_unflatten);
+use JSON::PP;
+use Try::Tiny;
+use File::Temp qw(tempfile);
+use Encode qw(find_encoding);
+
+use Zonemaster::Backend::Config;
+use Zonemaster::Engine;
+
+my $config = Zonemaster::Backend::Config->load_config();
+
+my $db_engine = $config->DB_engine;
+print "Configured database engine: $db_engine\n";
+
+if ( $db_engine =~ /^(MySQL|PostgreSQL|SQLite)$/ ) {
+    print( "Starting database migration\n" );
+
+    _update_result_entries( $config->new_DB()->dbh() );
+
+    print( "\nMigration done\n" );
+}
+else {
+    die "Unknown database engine configured: $db_engine\n";
+}
+
+
+sub _update_result_entries {
+    my ( $dbh ) = @_;
+
+    $dbh->do(<<SQL) or die 'Migration failed';
+        UPDATE result_entries
+           SET module = 'Backend_Test_Agent'
+         WHERE module = 'BACKEND_TEST_AGENT';
+SQL
+}

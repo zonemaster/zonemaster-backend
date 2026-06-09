833981e150efa0cf87ec5da4e372379a3b17ec23
Mattias Päivärinta <mattias.paivarinta@iis.se>
2026-06-09 15:41:28 +0200

    Make migration script idempotent

--- share/patch/patch_db_schema_version_1.pl
+++ share/patch/patch_db_schema_version_1.pl
@@ -5,17 +5,23 @@ use warnings;
 use Readonly;
 use Zonemaster::Backend::Config;
 
-Readonly my $EXPECTED_SCHEMA_VERSION => 0;
+Readonly my $TARGET_SCHEMA_VERSION   => 1;
+Readonly my $EXPECTED_SCHEMA_VERSION => $TARGET_SCHEMA_VERSION - 1;
 
 my $config = Zonemaster::Backend::Config->load_config();
 say "Configured database engine: ", $config->DB_engine;
 
 my $db               = $config->new_DB();
 my $detected_version = $db->get_schema_version();
+say "Target database schema version: ",        $TARGET_SCHEMA_VERSION;
 say "Expected pre-migration schema version: ", $EXPECTED_SCHEMA_VERSION;
 say "Detected database schema version: ",      $detected_version;
 
-if ( $detected_version ne $EXPECTED_SCHEMA_VERSION ) {
+if ( $detected_version eq $TARGET_SCHEMA_VERSION ) {
+    say "Schema already at target version.";
+    exit 0;
+}
+elsif ( $detected_version ne $EXPECTED_SCHEMA_VERSION ) {
     say "Schema version requirement not met!";
     exit 2;
 }
@@ -30,6 +36,6 @@ $db->dbh->do(
         CHECK (version >= 1)
     )"
 );
-$db->dbh->do( "INSERT INTO schema_version (id, version) VALUES (1, 1)" );
+$db->dbh->do( "INSERT INTO schema_version (id, version) VALUES (1, ?)", {}, $TARGET_SCHEMA_VERSION );
 
 say "Migration done";
